027bb723bb7818e380c442ecca7d6f03d07acdad
barmoshe <1barmoshe1@gmail.com>
2022-08-20 15:25:25 +0300

    test

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -41,12 +41,7 @@ void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRat
 
 void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
 {
-    // Your audio-processing code goes here!
-
-    // For more details, see the help for AudioProcessor::getNextAudioBlock()
-
-    // Right now we are not producing any data, in which case we need to clear the buffer
-    // (to prevent the output of random noise)
+   
     bufferToFill.clearActiveBufferRegion();
 }
 
